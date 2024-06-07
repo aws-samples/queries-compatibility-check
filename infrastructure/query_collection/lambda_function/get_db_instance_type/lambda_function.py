422f70d74d1bccef645b117c41ef0f3002b676f0
jiatin <jiatin@amazon.com>
2024-06-07 16:28:05 +0800

    support RDS single instance

--- infrastructure/query_collection/lambda_function/get_db_instance_type/lambda_function.py
+++ infrastructure/query_collection/lambda_function/get_db_instance_type/lambda_function.py
@@ -20,13 +20,12 @@ def calculate_instance_count_by_db_class(instance_class):
     pattern = r'^\d+'
     result = re.match(pattern, class_parts)
     if result:
-        return int(int(result.group())/2)
+        return int(int(result.group()) / 2)
     else:
         return 1
 
 
 def get_eni_for_ip(ip):
-    
     eni = {
         'eni_id': {
             'S': ''
@@ -35,7 +34,7 @@ def get_eni_for_ip(ip):
             'S': ''
         }
     }
-    
+
     response = ec2_client.describe_network_interfaces(
         Filters=[
             {
@@ -47,21 +46,19 @@ def get_eni_for_ip(ip):
         ],
         DryRun=False,
     )
-    
+
     if len(response['NetworkInterfaces']) > 0:
-        
         eni['eni_id']['S'] = response['NetworkInterfaces'][0]['NetworkInterfaceId']
-        eni['subnet_id']['S']  = response['NetworkInterfaces'][0]['SubnetId']
-        
+        eni['subnet_id']['S'] = response['NetworkInterfaces'][0]['SubnetId']
+
         return eni
-        
+
     return None
 
 
 def lambda_handler(event, context):
-    
     ip_list = []
-    
+
     database_info = {
         'endpoint': '',
         'read_endpoint': '',
@@ -69,38 +66,44 @@ def lambda_handler(event, context):
         'error': '',
         'instances': []
     }
-    
+
     cluster_identifier = event['cluster_identifier']
     traffic_window = event['traffic_window']
-    
+
     # Describe the database cluster
     try:
         response = rds.describe_db_clusters(
             DBClusterIdentifier=cluster_identifier
         )
-        
+
+        cluster = response['DBClusters'][0]
+
+        database_info['endpoint'] = cluster['Endpoint']
+        database_info['read_endpoint'] = cluster['ReaderEndpoint']
+
+        instances_info = rds.describe_db_instances(
+            Filters=[
+                {
+                    'Name': 'db-cluster-id',
+                    'Values': [
+                        cluster_identifier,
+                    ]
+                },
+            ],
+        )
+
     except Exception as e:
-        print(e)
-        database_info['error'] = 'endpoint error found'
-        
-        return database_info
-    
-    cluster = response['DBClusters'][0]
-    
-    database_info['endpoint'] = cluster['Endpoint']
-    database_info['read_endpoint'] = cluster['ReaderEndpoint']
-    
-    instances_info = rds.describe_db_instances(
-        Filters=[
-            {
-                'Name': 'db-cluster-id',
-                'Values': [
-                    cluster_identifier,
-                ]
-            },
-        ],
-    )
-    
+        try:
+            # Handle RDS single instance case
+            instances_info = rds.describe_db_instances(
+                DBInstanceIdentifier=event['cluster_identifier'],
+            )
+
+        except Exception as e:
+            database_info['error'] = 'endpoint error found'
+
+            return database_info
+
     for instance_info in instances_info['DBInstances']:
         instance = {
             'M': {
@@ -115,20 +118,22 @@ def lambda_handler(event, context):
                 },
             }
         }
-        
+
         ip = get_ip_for_database_endpoint(instance['M']['endpoint']['S'])
-        instance['M']['ip'] = { 'S': ip }
-        
-        instance['M']['eni'] = { 'M': get_eni_for_ip(ip) }
-        
+        instance['M']['ip'] = {'S': ip}
+
+        instance['M']['eni'] = {'M': get_eni_for_ip(ip)}
+
         database_info['instances'].append(instance)
-        database_info['instance_count'] = database_info['instance_count'] + calculate_instance_count_by_db_class(instance['M']['class']['S'])
-        
+        database_info['instance_count'] = database_info['instance_count'] + calculate_instance_count_by_db_class(
+            instance['M']['class']['S'])
+
     now = datetime.now()
     stop_time = now + timedelta(hours=traffic_window)
-    iso_time_with_utc_offset = '{}+00:00'.format(stop_time.isoformat())        
-    
+    iso_time_with_utc_offset = '{}+00:00'.format(stop_time.isoformat())
     database_info['stop_time'] = iso_time_with_utc_offset
-    
+
+    print('database_info: ')
+    print(database_info)
     return database_info
 
